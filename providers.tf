eadbc4fd8613c1c1cca8320667437cf4084d354c
saikiran <saikiran.merugu74@gmail.com>
2024-03-28 14:39:06 -0400

    added azure subscription details

--- providers.tf
+++ providers.tf
@@ -9,10 +9,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>2.0"
-      subscription_id   = "488a119a-b2e2-49eb-abfa-98c93480fc34"
-      tenant_id         = "ed236744-d848-41e9-a7cd-e801c29208fc"
-      client_id         = "9c0da9e1-33dc-4044-bc8a-854f91be2c72"
-      client_secret     = "eFV8Q~OIaTxwFYd8yqtkHZt9cp5Yk8rGoAEQfc2-"
+
     }
     random = {
       source  = "hashicorp/random"
@@ -20,7 +17,10 @@ terraform {
     }
   }
 }
-
 provider "azurerm" {
   features {}
+    subscription_id   = "488a119a-b2e2-49eb-abfa-98c93480fc34"
+    tenant_id         = "ed236744-d848-41e9-a7cd-e801c29208fc"
+    client_id         = "9c0da9e1-33dc-4044-bc8a-854f91be2c72"
+    client_secret     = "eFV8Q~OIaTxwFYd8yqtkHZt9cp5Yk8rGoAEQfc2-"
 }
