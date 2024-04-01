5763eb68a6ffa0e50a8431123a8e1584b162b6b5
saikiran <saikiran.merugu74@gmail.com>
2024-03-31 22:45:22 -0400

    updated jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,9 +4,6 @@ pipeline {
         SUBSCRIPTION_ID="488a119a-b2e2-49eb-abfa-98c93480fc34"
         TENANT_ID="ed236744-d848-41e9-a7cd-e801c29208fc"
         CLIENT_SECRET="eFV8Q~OIaTxwFYd8yqtkHZt9cp5Yk8rGoAEQfc2-"
-        PATH = " /home/ubuntu/.local/lib/python3.10/site-packages:$PATH"
-        
-
     }
     agent any
     stages {
